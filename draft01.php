01b8fa3f5cb3183880f18f7673fd9c39fc193d31
VictorElias2 <victorfalciroli966@gmail.com>
2024-08-27 20:17:14 -0300

    first commit

---
+++ draft01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Draft 1 - Using variables and handling it.</title>
+</head>
+<body>
+	<h1>Using variables</h1>
+	<p>All code runing on XAMPP or Laragon.</p>
+
+	<?php
+
+		echo "Teste com texto";
+
+
+	?>
+
+</body>
+</html>
